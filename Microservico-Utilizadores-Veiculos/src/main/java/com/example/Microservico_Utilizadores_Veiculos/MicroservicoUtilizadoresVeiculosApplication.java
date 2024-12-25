f672d1c06e39635118d33a0fe92d0b22551a81b5
FranGoncal <franciscogoncalvesep@gmail.com>
2024-12-25 16:55:36 +0000

    Init Microservico de Gestão de Utilizadores e Veiculos

---
+++ Microservico-Utilizadores-Veiculos/src/main/java/com/example/Microservico_Utilizadores_Veiculos/MicroservicoUtilizadoresVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Microservico_Utilizadores_Veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicoUtilizadoresVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicoUtilizadoresVeiculosApplication.class, args);
+	}
+
+}
---
+++ Microservico-Utilizadores-Veiculos/src/test/java/com/example/Microservico_Utilizadores_Veiculos/MicroservicoUtilizadoresVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Microservico_Utilizadores_Veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicoUtilizadoresVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
