cd282b60a8330434559ef6fd19b1f3aa5a7e23f2
FranGoncal <franciscogoncalvesep@gmail.com>
2024-12-25 15:47:29 +0000

    Init Micros servico OPC

---
+++ OPC/src/main/java/com/example/OPC/OpcGestaoDePontosApplication.java
@@ -0,0 +1,13 @@
+package com.example.OPC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpcGestaoDePontosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpcGestaoDePontosApplication.class, args);
+	}
+
+}
