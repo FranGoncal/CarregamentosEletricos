19e46d15e09acad32045f65a9ee66835ea3ed7a7
FranGoncal <franciscogoncalvesep@gmail.com>
2024-12-25 18:06:35 +0000

    Init Microservico de Simulação de Sessões de Carregamento

---
+++ Microservico-Simulacao-Sessoes-Carregamento/src/main/java/com/example/Microservico_Simulacao_Sessoes_Carregamento/MicroservicoSimulacaoSessoesCarregamentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Microservico_Simulacao_Sessoes_Carregamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicoSimulacaoSessoesCarregamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicoSimulacaoSessoesCarregamentoApplication.class, args);
+	}
+
+}
---
+++ Microservico-Simulacao-Sessoes-Carregamento/src/test/java/com/example/Microservico_Simulacao_Sessoes_Carregamento/MicroservicoSimulacaoSessoesCarregamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Microservico_Simulacao_Sessoes_Carregamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicoSimulacaoSessoesCarregamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
