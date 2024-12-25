42263a77bd1c9ce6024fbd3c0b04d91b26c13541
FranGoncal <franciscogoncalvesep@gmail.com>
2024-12-25 19:57:16 +0000

    Init Microservico de CEME e Faturação

---
+++ Microservico-CEME-Faturacao/src/main/java/com/example/Microservico_CEME_Faturacao/MicroservicoCemeFaturacaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Microservico_CEME_Faturacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicoCemeFaturacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicoCemeFaturacaoApplication.class, args);
+	}
+
+}
